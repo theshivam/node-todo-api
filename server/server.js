c1ca3a951dc8345ffd3e2a19f7f90c7e1561c0b0
Shivam Agarwal <shivam.agarwal18@outlook.com>
2018-05-10 03:29:41 +0530

    -> Adding Test cases for all APIs -> Adding GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -18,6 +19,23 @@ app.post('/todos',(req,res) => {
     res.send(doc);
   },(e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    res.status(400).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo){
+      res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
