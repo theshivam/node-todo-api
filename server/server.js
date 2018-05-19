b07ad52f0b5e9a875dd6bfddd3ceb50e5593f08d
Shivam Agarwal <shivam.agarwal18@outlook.com>
2018-05-19 17:55:22 +0530

    Adding DELETE /users/me/logout route

--- server/server.js
+++ server/server.js
@@ -116,6 +116,13 @@ app.post('/users',(req,res) => {
   });
 });
 
+app.delete('/users/me/logout',authenticate,(req,res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  },() => {
+    res.status(400).send();
+  });
+});
 
 app.get('/users/me',authenticate,(req,res) => {
   res.send(req.user);
